d0fca58a3a9eb1adc6fdcc4c862214f6fe68f7dc
Eric Thomas <erict3742@gmail.com>
2018-06-14 23:53:23 -0700

    Test conflict with String

--- app/src/main/java/net/cox/mario_000/disneylandpressedpennies/allCoinsAdapter.java
+++ app/src/main/java/net/cox/mario_000/disneylandpressedpennies/allCoinsAdapter.java
@@ -58,7 +58,7 @@ public class allCoinsAdapter extends ArrayAdapter<Coin> {
         inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         newCoins = getContext().getResources().getStringArray(R.array.new_coins);
         offMachine = getContext().getResources().getStringArray(R.array.off_mac);
-        String a = "Test";
+        int j = 3;
     }
 
     public void addSeparatorItem(final String item) {
