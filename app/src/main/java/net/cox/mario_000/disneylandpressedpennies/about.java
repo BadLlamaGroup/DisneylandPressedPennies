5c1da685d48744ea525059c683b276713d0de939
Eric Thomas <erict3742@gmail.com>
2018-08-02 21:18:07 -0700

    About code style

--- app/src/main/java/net/cox/mario_000/disneylandpressedpennies/about.java
+++ app/src/main/java/net/cox/mario_000/disneylandpressedpennies/about.java
@@ -19,34 +19,39 @@ import static net.cox.mario_000.disneylandpressedpennies.MainActivity.fragmentMa
  * Created by mario_000 on 2/10/2017.
  */
 
-public class about extends Fragment {
+public class about extends Fragment
+{
     private Tracker mTracker;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setTitle("About Disneyland Pressed Coins");
-        View view = inflater.inflate(R.layout.about, container, false);
-        TextView txtVersion = (TextView) view.findViewById(R.id.txtVersion);
-        txtVersion.setText("Version: " + BuildConfig.VERSION_NAME);
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        getActivity().setTitle( "About Disneyland Pressed Coins" );
+        View view = inflater.inflate( R.layout.about, container, false );
+        TextView txtVersion = view.findViewById( R.id.txtVersion );
+        txtVersion.setText( "Version: " + BuildConfig.VERSION_NAME );
 
-        MainActivity application = (MainActivity) getActivity();
+        MainActivity application = ( MainActivity ) getActivity();
         mTracker = application.getDefaultTracker();
-        mTracker.setScreenName("Page - About");
-        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        mTracker.setScreenName( "Page - About" );
+        mTracker.send( new HitBuilders.ScreenViewBuilder().build() );
 
-        Button btnExit = (Button) view.findViewById(R.id.btnAboutExit);
-        btnExit.setOnClickListener(new View.OnClickListener() {
+        Button btnExit = view.findViewById( R.id.btnAboutExit );
+        btnExit.setOnClickListener( new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick( View view )
+            {
                 fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 MainPage fragment = new MainPage();
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.replace(R.id.mainFrag, fragment, "main");
+                fragmentTransaction.addToBackStack( null );
+                fragmentTransaction.replace( R.id.mainFrag, fragment, "main" );
                 fragmentTransaction.commit();
             }
-        });
+        } );
 
         return view;
     }
